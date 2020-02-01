2531bd7ec5c57e5be3f500e98eafe37ae213ea6b
djeriko <jerikosalivan@gmail.com>
2020-02-01 15:34:48 +0900

    added prefs

---
+++ Assets/Scripts/PlatformerGayNew2D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class PlatformerGayNew2D : PlatformerCharacter2D 
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        var buttonScript = other.gameObject.GetComponent<ButtonScript>();     
+        if (buttonScript != null) {
+            buttonScript.ButtonTest();
+        }
+    }
+}
