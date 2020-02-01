eb33f3f27a2b2e7a856df4b7f4b4be3600b8d504
djeriko <jerikosalivan@gmail.com>
2020-02-01 17:31:24 +0900

    added elevator to prefabs

--- Assets/Scripts/PlatformerGayNew2D.cs
+++ Assets/Scripts/PlatformerGayNew2D.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets._2D;
 
-public class PlatformerGayNew2D : PlatformerCharacter2D 
+public class PlatformerGayNew2D : PlatformerCharacter2D, IActionPirate
 {
+    public void Action(string command)
+    {
+    }
+
     void OnTriggerExit2D(Collider2D other)
     {
-        var buttonScript = other.gameObject.GetComponent<ButtonScript>();     
-        if (buttonScript != null) {
-            buttonScript.ButtonTest();
+        if (other.gameObject.tag == "Button")
+        {
+            var buttonScript = other.gameObject.GetComponent<ButtonScript>();
+            if (buttonScript != null)
+            {
+                buttonScript.ButtonTest();
+            }
         }
     }
 }
