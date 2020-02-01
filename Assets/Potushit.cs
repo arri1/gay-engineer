fcc8cd4f07d7ce54a6517746c7c286db584309a3
djeriko <jerikosalivan@gmail.com>
2020-02-02 07:40:45 +0900

    fixed onFire

--- Assets/Potushit.cs
+++ Assets/Potushit.cs
@@ -4,43 +4,13 @@ using UnityEngine;
 
 public class Potushit : ActionMother
 {
-    [SerializeField]
-    string command;
-    // Field to link target element
-    [SerializeField]
-    ActionMother target;
-    
-    [SerializeField]
-    float duration=1;
-    bool isBlocked = false;
-    public GameObject encounter;
-    
-  
-    // Start is called before the first frame update
-    public override void Action(string command){
-        switch(command){
-            case "fire":
-            PotushitFire();
-            break;
-            default : print("def");break;
-        }
-    }
-        void PotushitFire() {
-        //  if (!isBlocked)
-        // {
-            encounter.SetActive(false);
-            // target.Action(command);
-            print("hello world from Potushit");
-            // StopAllCoroutines();
-            // StartCoroutine(timer());
-            // isBlocked = true;
-          
-        // }
-        
-      }
-       IEnumerator timer()
+    public override void Action(string command)
     {
-        yield return new WaitForSeconds(duration);
-        isBlocked = false;
+        PotushitFire();
     }
+    void PotushitFire()
+    {
+        gameObject.SetActive(false);
+        print("hello world from Potushit");
     }
+}
