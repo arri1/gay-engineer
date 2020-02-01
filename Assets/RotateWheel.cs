8d4daa99596948e6e88e226d3f4144bbc26f976b
djeriko <jerikosalivan@gmail.com>
2020-02-01 18:43:23 +0900

    added wheel

---
+++ Assets/RotateWheel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWheel : ActionMother
+{
+    float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(Vector3.forward * speed * Time.deltaTime); 
+    }
+
+    void Rotate(){
+        speed = speed + 40.0f;
+    }
+}
