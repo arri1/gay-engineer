65da7edd0be79a18fc567f6760017b1c1bcfb088
arri <ewws@mail.ru>
2020-02-01 17:51:41 +0900

    added ElevatorScript

---
+++ Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : ActionMother
+{
+    [SerializeField] private List<Transform> points;
+
+    private int current=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Action(string command)
+    {
+        
+    }
+
+    void Up()
+    {
+        //if (current<)
+    }
+}
